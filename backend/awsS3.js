4fa3bbb592fb1e747fe77f223ce4e17f3eec32a8
christopher-hauser <chauser6@gmail.com>
2023-09-05 15:54:46 -0400

    Fix AWS issues for photo storage.

--- backend/awsS3.js
+++ backend/awsS3.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
 // name of your bucket here
-const NAME_OF_BUCKET = "aa-rarebnb";
+const NAME_OF_BUCKET = "rare-bnb-storage-bucket";
 
 const multer = require("multer");
 
