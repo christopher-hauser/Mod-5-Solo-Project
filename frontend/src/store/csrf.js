46b9b7fe4a6f24f2d4fddac09e5903317e324c92
christopher-hauser <chauser6@gmail.com>
2023-09-08 13:31:43 -0400

    CSRF API endpoint rework.

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -3,23 +3,24 @@ import Cookies from 'js-cookie';
 export async function csrfFetch(url, options = {}) {
     options.method = options.method || 'GET';
     options.headers = options.headers || {};
-
-    const csrfRes = await window.fetch('/csrf').then(csrf => csrf.csrfToken);
-    console.log(csrfRes);
-
-    if (options.method.toUpperCase() !== 'GET') {
-        if (options.headers["Content-Type"] === "multipart/form-data") {
-            delete options.headers["Content-Type"];
-        } else {
-            options.headers["Content-Type"] =
-                options.headers["Content-Type"] || "application/json";
-        }
-        options.headers['XSRF-Token'] = csrfRes;
-    }
-
     url = process.env.NODE_ENV === 'development' ? url : `https://aa-rarebnb.onrender.com${url}`;
 
-    const res = await window.fetch(url, options);
+    const res = await window.fetch('/csrf').then(csrf => {
+        return csrf.body.csrfToken;
+    }).then(csrfRes => {
+
+        if (options.method.toUpperCase() !== 'GET') {
+            if (options.headers["Content-Type"] === "multipart/form-data") {
+                delete options.headers["Content-Type"];
+            } else {
+                options.headers["Content-Type"] =
+                    options.headers["Content-Type"] || "application/json";
+            }
+            options.headers['XSRF-Token'] = csrfRes;
+        }
+
+        return window.fetch(url, options);
+    });
 
     if (res.status >= 400) throw res;
 
